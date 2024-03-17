6b00c5a2f0f78dfb17fd8c4c12450d91ce77ff7b
SAYAM SIDHARTH <81490535+sidharthSayam@users.noreply.github.com>
2024-03-17 23:48:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const hour = document.getElementById("hours");
+const min = document.getElementById("minutes");
+const sec = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "Am";
+
+  if (h > 12) {
+    h = 12 - h;
+    ampm = "PM";
+  }
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hour.innerText = h;
+  min.innerText = m;
+  sec.innerText = s;
+  ampm.innerText = ampm;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+updateClock();
