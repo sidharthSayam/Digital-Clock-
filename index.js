72a2043a5bec7fa7dec732e01fa6317c8e3c2b94
SAYAM SIDHARTH <81490535+sidharthSayam@users.noreply.github.com>
2024-03-18 00:05:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function updateClock() {
   let ampm = "Am";
 
   if (h > 12){
-    h = h - 12
-    ampm = "PM"
+    h = h - 12;
+    ampm = "PM";
   }
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
